89efb0d7663158d86e9b8125379245e9d1f8e2de
Adam Breaux <breauxaj@gmail.com>
2016-05-31 15:27:31 -0400

    Initial commit

---
+++ manifests/params.pp
@@ -0,0 +1,20 @@
+# Class: haveged::params
+#
+# This class set parameters used in this module
+#
+# Actions:
+#   - Defines numerous parameters used by other classes
+#
+class haveged::params {
+  $haveged_package_ensure = 'latest'
+
+  case $::operatingsystem {
+    'CentOS', 'Debian', 'RedHat': {
+      $haveged_package = 'haveged'
+      $haveged_service = 'haveged'
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+    }
+  }
+}
